9d73196f225fb1f82984c827bd5c75244ee529e1
pacokwon <haechank@gmail.com>
2020-01-07 12:06:57 +0900

    Rewrite Client class
    Rewrite Client class. Lots of other data needed to be sent to the
    client, so fields were separated to 4

--- client.py
+++ client.py
@@ -1,39 +1,67 @@
+from functools import partial
 from skullconstants import HEADER_SIZE, HOST, PORT
 import pickle
+import select
 import socket
 
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+    client.setblocking(False)
     while True:
-        pass
+        readable, _, _ = select.select([client], [], [])
+
+        if readable:
+            message = receive_data(client)
+            operate_action(client, message)
 
 
-def receive_message(client):
+def operate_action(client, data):
+    if data["type"] == "in":
+        is_valid = partial(data["validator"], data["payload"])
+        while True:
+            chosen = input(data["message"])
+            if is_valid(chosen):
+                break
+        send_data(client, {"payload": int(chosen)})
+
+    else:
+        print(data["message"])
+
+
+def receive_data(client):
+    """
+    Receive data from the server
+
+    params:
+        client: a client socket
+    returns:
+        a python dictionary that follows the rules of 'data'
+        as described in the 'send' function of player.py
+    """
     try:
-        message_size = client.recv(HEADER_SIZE)
-        if not len(message_size):
+        data_size = client.recv(HEADER_SIZE)
+        if not len(data_size):
             return False
 
-        message_size = int(message_size.decode("utf-8"))
-        return pickle.loads(client.recv(message_size))
+        data_size = int(data_size.decode("utf-8"))
+        return pickle.loads(client.recv(data_size))
 
     except:
         return False
 
 
-def send_message(client, data):
+def send_data(client, data):
     """
-    send message to client
+    send data to client
     params:
-        client: client socket
+        client: a client socket
         data: python dictionary
     """
     pickled = pickle.dumps(data)
-    encoded_data = pickled.encode("utf-8")
-    header = f"{len(encoded_data):<{HEADER_SIZE}}".encode("utf-8")
-    client.send(header + encoded_data)
+    header = f"{len(pickled):<{HEADER_SIZE}}".encode("utf-8")
+    client.send(header + pickled)
 
 
 if __name__ == "__main__":
